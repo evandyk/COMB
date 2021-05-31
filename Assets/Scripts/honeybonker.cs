568fea9a4f30a38ef030921958fc8c689becd5ac
evandyk <eric.vandyk@utexas.edu>
2021-05-30 20:01:16 -0700

    Adding swipe script, hit detection, and sound

--- Assets/Scripts/honeybonker.cs
+++ Assets/Scripts/honeybonker.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Honeybonker : MonoBehaviour
@@ -7,9 +9,13 @@ public class Honeybonker : MonoBehaviour
     public Camera fpsCam;
     public AudioClip bonkSound;
     public AudioClip swishSound;
-    public float range = 5f;
+    public AudioClip swipeBonkSound;
+    public float bonkRange = 3f;
+    public float swipeRange = 5f;
     public float fireRate = 2f;
-    public float weaponDamage = 10f;
+    public float bonkDamage = 10f;
+    public float swipeDamage = 2f;
+    public int maxSwipeBonks = 5;
 
     AudioSource source;
     private float nextTimeToFire;
@@ -32,21 +38,20 @@ public class Honeybonker : MonoBehaviour
         if (Input.GetMouseButtonDown(1))
         {
             m_Animator.SetTrigger("Rclick");
+            source.PlayOneShot(swishSound, 0.7f);
         }
     }
 
     void Bonk()
     {
         RaycastHit hit;
-        if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
+        if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, bonkRange))
         {
-            
-
             Target target = hit.transform.GetComponent<Target>();
             if (target != null)
             {
                 source.PlayOneShot(bonkSound, 0.7f);
-                target.TakeDamage(weaponDamage, hit);
+                target.TakeDamage(bonkDamage, hit);
                 Debug.Log(hit.transform.name);
             }
         }
@@ -56,6 +61,36 @@ public class Honeybonker : MonoBehaviour
 
     void Swipe()
     {
+        // 7 is Enemy Layer Mask. This prevents the player from being detected as a hit
+        RaycastHit[] hits = Physics.SphereCastAll(fpsCam.transform.position, 1, fpsCam.transform.forward, swipeRange);
+        if (hits != null)
+        {
+            List<RaycastHit> toBonk = new List<RaycastHit>();
+            foreach (RaycastHit hit in hits)
+            {
+                Target t = hit.transform.gameObject.GetComponent<Target>();
+                Vector3 vectorToCollider = (hit.transform.position - fpsCam.transform.position);
+                vectorToCollider.Normalize();
 
+                if (t != null && (Vector3.Dot(vectorToCollider, fpsCam.transform.forward) > .4) && toBonk.Count < maxSwipeBonks)
+                {
+                    Debug.Log("Swipe hit");
+                    toBonk.Add(hit);
+                }
+            }
+
+            StartCoroutine(MultiBonk(toBonk));
+        }
+    }
+
+    private IEnumerator MultiBonk(List<RaycastHit> bonks)
+    {
+        foreach(RaycastHit bonk in bonks)
+        {
+            Target t = bonk.transform.gameObject.GetComponent<Target>();
+            source.PlayOneShot(swipeBonkSound, 0.7f);
+            t.TakeDamage(swipeDamage, bonk);
+            yield return new WaitForSeconds(.05f);
+        }
     }
 }
